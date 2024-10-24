196a21108474c34da4fb67d73773e2cf42590b1f
torealdemir <aldemirtore@gmail.com>
2024-10-24 15:07:44 +0300

    some more arrangements on body section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 const Features = () => {
   return (
-    <section id="features">
-      <h3>Our Features</h3>
-      <ul>
-        <li>Data-Driven Planning</li>
-        <li>Community Engagement</li>
-        <li>Sustainability Initiatives</li>
-      </ul>
-    </section>
+    <div className='d-flex align-items-center justify-content-center bg-warning text-secondary'>
+         <section id="features ">
+            <h3>Our Features</h3>
+            <ul>
+                <li>Data-Driven Planning</li>
+                <li>Community Engagement</li>
+                <li>Sustainability Initiatives</li>
+            </ul>
+        </section>
+    </div>
+   
   );
 };
 
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 const HeroSection = () => {
   return (
-    <section className="hero">
+    <section className="position-relative">
+  <div className="vw-60">
+    <img 
+      className="img-fluid" 
+      src="https://images.unsplash.com/photo-1509453721491-c3af5961df76?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+      alt="City Planning" 
+    />
+    <div className="position-absolute top-50 start-50 translate-middle text-white text-center">
       <h2>Building Sustainable Cities Together</h2>
       <p>Join us in planning for a better future.</p>
-      <a href="#features" className="cta-button">Learn More</a>
-    </section>
+      <a href="#features" className="btn btn-primary">Learn More</a>
+    </div>
+  </div>
+</section>
   );
 };
 
