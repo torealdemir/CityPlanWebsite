8cb4f16f9ee239be46e83ff24fcda3d9a7a095f4
torealdemir <aldemirtore@gmail.com>
2024-11-08 18:14:22 +0300

    feat: map component fixed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Fix marker icons for mobile compatibility
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const Map = () => {
   const position = [39.91676196084725, 32.8577149124137]; // Example coordinates
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: '400px', width: '100%' }}
+      tap={true} // Ensure touch events are recognized
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
