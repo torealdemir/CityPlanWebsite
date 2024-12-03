96d27b8dc6b7f2788a3c50c3a6ddc2582a10d61a
torealdemir <aldemirtore@gmail.com>
2024-12-03 16:35:50 +0300

    fix: more paragraphs added to main page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,15 @@ const Home = () => {
               alt="City Planning" 
             />
             <h2 className="display-4">Building Sustainable Cities Together</h2>
-            <p className="lead mb-4">Join us in planning for a better future.</p>
+            <p className="lead mb-4">Join us in planning for a better future.Lorem ipsum odor amet, consectetuer adipiscing elit. Elementum pellentesque mollis curae rhoncus arcu. Tincidunt consequat nisi porttitor ridiculus massa aliquet, cursus laoreet cursus. Natoque fermentum elit vulputate elit vestibulum tincidunt lacinia tempor amet. Nam diam senectus lacinia malesuada congue dictum. Ante montes eu augue vehicula pellentesque ultrices. Vivamus aliquet mus tellus quis quis.
+
+Fusce aptent nascetur sit vestibulum montes. Mi nostra pellentesque porttitor metus leo praesent sociosqu. Praesent volutpat venenatis phasellus morbi efficitur nulla. Ligula integer interdum donec; sed pellentesque senectus. Est habitasse mauris tellus erat eros, nibh ut egestas eget. In porta condimentum nam dis dapibus. Porta mi vel porttitor iaculis montes torquent tempus. Euismod dis taciti feugiat erat dolor parturient sollicitudin.
+
+Interdum scelerisque feugiat congue habitant parturient convallis. Laoreet eleifend posuere dolor duis euismod mollis aliquet. Facilisis interdum molestie class pulvinar velit aliquam. Eu orci metus dictum tincidunt malesuada ornare odio litora? Consequat convallis odio fusce himenaeos nam luctus. Commodo venenatis ipsum curabitur erat viverra quis ullamcorper mus hac. Sapien mauris praesent proin ornare ultricies quisque nam semper ad. Porttitor tempor porta fringilla; mauris montes luctus vitae. Lacus ac congue eget, tincidunt semper commodo aliquam arcu?
+
+Eros tincidunt mauris libero molestie class sem potenti suscipit. Turpis dignissim aliquam habitasse mus primis pellentesque senectus. Habitasse massa nulla litora per nam turpis massa scelerisque. Nunc viverra suscipit ridiculus tincidunt in mollis congue. Pulvinar suspendisse fusce cras taciti laoreet arcu consequat ornare. Risus nunc ipsum rutrum suspendisse aliquam magnis netus ad. Maecenas cubilia odio curae proin fermentum amet.
+
+Nam ultrices nullam ultrices dapibus dis interdum ipsum pulvinar. Dignissim ornare volutpat elit dui senectus arcu. Suscipit fames arcu convallis pulvinar habitant tellus. Bibendum magnis dignissim curabitur mauris lacus at elementum odio. Pellentesque adipiscing arcu ad maximus tincidunt at vehicula tempus consectetur. Proin penatibus quam senectus, iaculis quisque finibus sodales. Netus bibendum non ad primis ornare maximus quisque. Sem sollicitudin nunc varius mollis nibh rutrum laoreet primis.</p>
             <a href="#features" className="btn btn-primary btn-lg">Learn More</a>
           </div>
         </div>
